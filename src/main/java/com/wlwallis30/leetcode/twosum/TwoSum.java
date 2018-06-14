8203468fd195f94aa423e3deab9b1f160c4ec7ef
wlwallis30 <wlwallis30@gmail.com>
2018-06-14 12:54:42 -0700

    @15@ three sum

--- src/main/java/com/wlwallis30/leetcode/twosum/TwoSum.java
+++ src/main/java/com/wlwallis30/leetcode/twosum/TwoSum.java
@@ -4,8 +4,8 @@ package com.wlwallis30.leetcode.twosum;
  * Created by larrywang on 6/13/18.
  */
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+
 public class TwoSum {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -17,5 +17,27 @@ public class TwoSum {
             map.put(nums[i], i);
         }
         throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new LinkedList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int l = i + 1, r = nums.length - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                    while (++l < r && nums[l] == nums[l - 1]) ; // skip the same b
+                    while (--r > l && nums[r] == nums[r + 1]) ; // skip the same c
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    while (--r > l && nums[r] == nums[r + 1]) ;
+                } else {
+                    while (++l < r && nums[l] == nums[l - 1]) ;
+                }
+            }
+        }
+
+        return res;
     }
 }
