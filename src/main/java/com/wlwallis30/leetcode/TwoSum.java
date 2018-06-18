d10af94df50243c32b128bd69ada244a55039ddb
wlwallis30 <wlwallis30@gmail.com>
2018-06-18 12:26:20 -0700

    adding 167, 170

---
+++ src/main/java/com/wlwallis30/leetcode/TwoSum.java
@@ -0,0 +1,138 @@
+/*
+ * Created by larrywang in 2018
+ * Copyright (c) https://github.com/wlwallis30/ 2018. All rights reserved.
+ * Last modified on 6/15/18 1:37 AM
+ */
+package com.wlwallis30.leetcode;
+
+import java.lang.reflect.Array;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class TwoSum {
+  private Map<Integer, Integer> twoSumMap = new HashMap<>();
+  //private HashMap<Integer, Integer> xyz = new HashMap<>();
+
+  public void add(int num) {
+    twoSumMap.put(num, twoSumMap.get(num) + 1);
+  }
+
+  public Boolean find(int target) {
+    // if only interested in key, use for loop,
+    // Iterator it = mp.entrySet().iterator();  while (it.hasNext()) Map.Entry pair = (Map.Entry)it.next();
+    for (Integer key: twoSumMap.keySet()) {
+      int search = target - key;
+      if(search != key && twoSumMap.containsKey(search)
+          || search == key && twoSumMap.get(search) > 1) return true;
+    }
+    return false;
+  }
+
+  public int[] twoSum_1(int[] nums, int target) {
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
+      if (map.containsKey(complement)) {
+        return new int[] { map.get(complement), i };
+      }
+      map.put(nums[i], i);
+    }
+    throw new IllegalArgumentException("No two sum solution");
+  }
+
+  public List<List<Integer>> threeSum_15(int[] nums) {
+    List<List<Integer>> res = new LinkedList<>();
+    if (nums.length < 3) return res;
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 2; ++i) {
+      if (nums[i] > 0) break;
+      if (i > 0 && nums[i] == nums[i - 1]) continue;
+      int left = i + 1, right = nums.length - 1;
+      while (left < right) {
+        int tmpSum = nums[i] + nums[left] + nums[right];
+        if (tmpSum == 0) {
+          res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+          ++left;
+          while (nums[left] == nums[left - 1] && left < right)  ++left; // skip the same b
+          --right;
+          while (nums[right] == nums[right + 1] && left < right)  --right; // skip the same c
+        } else if (tmpSum > 0) {
+          --right;
+          while (nums[right] == nums[right + 1] && left < right) --right;
+        } else {
+          ++left;
+          while (nums[left] == nums[left - 1] && left < right) ++left;
+        }
+      }
+    }
+    return res;
+  }
+
+  // 3 sum closest
+  public int threeSum_16 (int[] nums, int target) {
+    if (nums.length < 3) return Integer.MAX_VALUE;
+    int closest = nums[0] + nums[1] + nums[2];
+    int diff = Math.abs(closest - target);
+    Arrays.sort(nums);
+    for (int i = 0; i < nums.length - 2; i++) {
+      int left = i + 1, right = nums.length - 1;
+      while(left < right) {
+        int sum = nums[i] + nums[left] + nums[right];
+        int tmpDiff = Math.abs(sum - target);
+        if(tmpDiff < diff) {
+          closest = sum;
+          diff = tmpDiff;
+        }
+        if(sum < target) left++;
+        else right--;
+      }
+    }
+    return closest;
+  }
+
+  public List<List<Integer>> fourSum_18(int[] nums, int target) {
+    List<List<Integer>> res = new LinkedList<>();
+    if (nums.length < 4) return res;
+    Arrays.sort(nums);
+    for(int i = 0; i < nums.length - 3; i++)
+      for (int j = i + 1; j < nums.length - 2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]) {
+          continue;
+        }
+        int left = j + 1, right = nums.length - 1;
+        while (left < right) {
+          int sum = nums[i] + nums[j] + nums[left] + nums[right];
+          if (sum == target) {
+            res.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+            ++left;
+            while (nums[left] == nums[left - 1] && left < right) ++left;
+            --right;
+            while (nums[right] == nums[right + 1] && left < right) --right;
+          } else if (sum > target) {
+            --right;
+            while (nums[right] == nums[right + 1] && left < right) --right;
+          } else {
+            ++left;
+            while (nums[left] == nums[left - 1] && left < right) ++left;
+          }
+        }
+      }
+    //List xyz = new LinkedList(new HashSet<>(res));this will make not ordered,
+    //Collections.sort(xyz) will throw since inside it is a ArrayList, not sortable
+    return res.stream().distinct().collect(Collectors.toList());
+  }
+
+  public int[] twoSumSorted_167(int[] nums, int target) {
+    int[] res = new int[]{};
+    int left = 0, right = nums.length - 1;
+    while(left < right) {
+      int tmpSum = nums[left] + nums[right];
+      if (tmpSum == target) {
+        res = new int[]{left + 1, right + 1};
+        return res;
+      } else if (tmpSum < target) ++left;
+      else --right;
+    }
+    return res;
+  }
+}
---
+++ src/test/java/com/wlwallis30/leetcode/TwoSumTest.java
@@ -0,0 +1,166 @@
+/*
+ *  Created by larrywang in 2018
+ *  Copyright (c) https://github.com/wlwallis30/ 2018. All rights reserved.
+ *  Last modified on 6/16/18 12:55 AM
+ */
+
+package com.wlwallis30.leetcode;
+
+import static org.junit.Assert.*;
+
+import java.util.LinkedList;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.List;
+
+public class TwoSumTest {
+  private TwoSum twoSum;
+  private Boolean debug;
+  private String resultStr;
+  private String expectedStr;
+  private String expectedNotEqual;
+  private String expectedIntStr;
+
+  @Before
+  public void setUp() throws Exception {
+    this.twoSum = new TwoSum();
+    this.debug = true;
+    this.resultStr = "result is: ";
+    this.expectedStr = "expected is: ";
+    this.expectedNotEqual = "not equal, expected is: ";
+    this.expectedIntStr = "not equal, expected integer is: %d";
+    System.out.println("Init before each test...");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    System.out.println("Done after each test");
+  }
+
+  private void printQuestionInfo (int methodIdxInClass) {
+    if(this.debug) {
+      System.out.println("----Now Testing----"
+          + TwoSum.class.getMethods()[methodIdxInClass].getName());
+    }
+  }
+
+  private void debugPrint(String message) {
+    if(this.debug) {
+      System.out.println(message);
+    }
+  }
+
+  private void debugPrint(int num) {
+    if(this.debug) {
+      System.out.println(String.format("integer is %d", num));
+    }
+  }
+
+  private void debugPrint(Object object) {
+    if(this.debug) {
+      System.out.print(object);
+    }
+  }
+
+  @Test
+  public void testTwoSum_1() {
+    int[] nums = { 2, 7, 11, 15};
+    int target = 9;
+    int[] res = twoSum.twoSum_1(nums, target);
+    int[] expected = {0, 1};
+    Object[] expectedObj = {expected};
+    Object[] resObj = {res};
+
+    this.printQuestionInfo(0);
+    this.debugPrint(this.expectedStr + Arrays.toString(expected));
+    this.debugPrint(this.resultStr + Arrays.toString(res));
+    assertTrue("should be [0, 1]", Arrays.deepEquals(expectedObj, resObj));
+  }
+
+  @Test
+  public void testThreeSum_15() {
+    int[] nums = {-1, 0, 1, 2, -1, -4};
+    List<List<Integer>> res = twoSum.threeSum_15(nums);
+    List<List<Integer>> expected = new LinkedList<>();
+    expected.add(Arrays.asList(-1, -1, 2));
+    expected.add(Arrays.asList(-1, 0, 1));
+
+    this.printQuestionInfo(1);
+    this.debugPrint(this.expectedStr + expected.toString());
+    this.debugPrint(this.resultStr + res.toString());
+    assertTrue(this.expectedNotEqual + expected.toString(),
+        expected.equals(res));
+  }
+
+  @Test
+  public void testThreeSumClosest_16() {
+    int[] nums = new int[]{-1, 2, 1, -4};
+    int target = 1;
+    int res = twoSum.threeSum_16(nums, target);
+    int expected = 2;
+
+    this.printQuestionInfo(2);
+    this.debugPrint(this.expectedStr + expected);
+    this.debugPrint(this.resultStr + res);
+    assertTrue(String.format(this.expectedIntStr, expected), res == expected);
+    nums = new int[]{-3,-2,-5,3,-4};
+    target = -1;
+    expected = -2;
+    res = twoSum.threeSum_16(nums, target);
+    this.debugPrint(this.expectedStr + expected);
+    this.debugPrint(this.resultStr + res);
+    assertTrue(String.format(this.expectedIntStr, expected), res == expected);
+  }
+
+  @Test
+  public void testFourSum_18() {
+    int[] nums = {-1, 0, 1, 2, -2, 0};
+    int target = 0;
+    List<List<Integer>> res = twoSum.fourSum_18(nums, target);
+    List<List<Integer>> expected = new LinkedList<>();
+    expected.add(Arrays.asList(-2, -1, 1, 2));
+    expected.add(Arrays.asList(-2,  0, 0, 2));
+    expected.add(Arrays.asList(-1,  0, 0, 1));
+
+    this.printQuestionInfo(3);
+    this.debugPrint(this.expectedStr + expected.toString());
+    this.debugPrint(this.resultStr + res.toString());
+    assertTrue(this.expectedNotEqual + expected.toString(),
+        expected.equals(res));
+
+    nums = new int[]{0, 0, 0, 0};
+    target = 1;
+    res = twoSum.fourSum_18(nums, target);
+    expected.clear();
+    assertTrue(this.expectedNotEqual + expected.toString(),
+        expected.equals(res));
+
+    nums = new int[]{-1,-5,-5,-3,2,5,0,4};
+    target = -7;
+    expected.clear();
+    expected.add(Arrays.asList(-5,-5,-1,4));
+    expected.add(Arrays.asList(-5,-3,-1,2));
+    res = twoSum.fourSum_18(nums, target);
+    this.debugPrint(this.expectedStr + expected);
+    this.debugPrint(this.resultStr + res);
+    assertTrue(this.expectedNotEqual + expected.toString(),
+        expected.equals(res));
+  }
+
+  @Test
+  public void testTwoSum_167() {
+    int[] nums = {2, 7, 11, 15};
+    int target = 9;
+    int[] res = twoSum.twoSumSorted_167(nums, target);
+    int[] expected = {1, 2};
+    Object[] expectedObj = {expected};
+    Object[] resObj = {res};
+
+    this.printQuestionInfo(4);
+    this.debugPrint(this.expectedStr + Arrays.toString(expected));
+    this.debugPrint(this.resultStr + Arrays.toString(res));
+    assertTrue("should be [0, 1]", Arrays.deepEquals(expectedObj, resObj));
+  }
+}
