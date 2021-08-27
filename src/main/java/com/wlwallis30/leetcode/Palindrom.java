d95e0f48da10cb189be0e1073d68c00637872ebb
wlwallis30 <wlwallis30@gmail.com>
2021-08-26 20:45:05 -0700

    adding palindrome's 3 problems

--- src/main/java/com/wlwallis30/leetcode/Palindrom.java
+++ src/main/java/com/wlwallis30/leetcode/Palindrom.java
@@ -1,7 +1,9 @@
-package com.wlwallis30.leetcode;
+  package com.wlwallis30.leetcode;
+
+import java.util.*;
 
 public class Palindrom {
-  public String palindrom_5(String str) {
+  public String palindromLongSubstr_5(String str) {
     if(str.isEmpty()) return "".toString();
     String res = "";
     int leftPalin = 0, rightPalin = 0;
@@ -28,5 +30,87 @@ public class Palindrom {
     // java's sub string: extend to char whose index = endIndex-1
     res = str.substring(leftPalin+0, rightPalin+1);
     return res;
+  }
+
+  public boolean palindromePermutation_266(String str) {
+    Map<Character, Integer> charCnt = new HashMap<>();
+    for (char chartmp: str.toCharArray()) {
+      if(charCnt.containsKey(chartmp)) {
+        Integer tmpVal = charCnt.get(chartmp);
+        charCnt.put(chartmp, ++tmpVal);
+      }
+      else {
+        charCnt.put(chartmp, 1);
+      }
+      // the above better be replaced by : map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+    }
+    int oddCnt = 0;
+    for (char chartmp: charCnt.keySet()) {
+      //      System.out.println("one key:" + chartmp);
+      if(charCnt.get(chartmp) % 2 == 1) oddCnt++;
+    }
+    //    System.out.println("odd cnt:" + oddCnt);
+    return (oddCnt==0 || oddCnt==1);
+   /*  single pass and using an int array map
+   * int[] map = new int[128];
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            map[s.charAt(i)]++;
+            if (map[s.charAt(i)] % 2 == 0)
+                count--;
+            else
+                count++;
+        }
+        return count <= 1;
+   */
+  }
+  Set<String> set = new HashSet<>();
+  public List<String> generatePalindromes_267(String str) {
+    int[] charCnt = new int[128];
+    char[] halfStr = new char[str.length() / 2];
+    for (char tmpCha: str.toCharArray()) { ++charCnt[tmpCha]; }
+
+    // a null char, you can NOT use '' as other languages can, https://stackoverflow.com/questions/18410234/how-does-one-represent-the-empty-char/18410258
+    char midChar = 0;
+    String midStr = "";
+    String halfStrBuild = "";
+    int k = 0;
+    for (int i = 0; i < charCnt.length; i++) {
+      if (charCnt[i] % 2 == 1) {
+        midStr += (char)i;// to judge if it is a panlindrome
+        midChar = (char) i;
+      }
+      if(midStr.length()>1) return new ArrayList<>();
+      char[] tmp= new char[charCnt[i]/2];
+      Arrays.fill(tmp, (char)i);
+      // java supper sucks,  halfStrBuild += tmp does not work at all!
+      halfStrBuild += new StringBuilder().append(tmp);
+    }
+    System.out.println(halfStrBuild);
+    halfStr = halfStrBuild.toCharArray();
+    System.out.println(halfStr);
+    permute(halfStr, 0, midChar);
+    return new ArrayList <String> (set);
+  }
+
+
+  void swap(char[] str, int i, int j) {
+    char temp = str[i];
+    str[i] = str[j];
+    str[j] = temp;
+  }
+
+  void permute(char[] str, int startIdx, char midChar) {
+    if (startIdx == str.length) {
+      set.add(new String(str) + (midChar == 0 ? "" : midChar) + new StringBuffer(new String(str)).reverse());
+    } else {
+      for (int i = startIdx; i < str.length; i++) {
+        if (startIdx != i && str[startIdx] == str[i]) continue;
+        swap(str, startIdx, i);
+        permute(str, startIdx + 1, midChar);
+        swap(str, startIdx, i);
+
+      }
+    }
   }
 }
