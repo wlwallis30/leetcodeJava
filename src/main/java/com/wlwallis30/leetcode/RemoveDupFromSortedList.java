53c94cad89fc75c34c1c3e5ed123a085ab79b761
wlwallis30 <wlwallis30@gmail.com>
2021-09-12 15:03:56 -0700

    adding 82

--- src/main/java/com/wlwallis30/leetcode/RemoveDupFromSortedList.java
+++ src/main/java/com/wlwallis30/leetcode/RemoveDupFromSortedList.java
@@ -11,4 +11,23 @@ public class RemoveDupFromSortedList {
     }
     return head;
   }
+
+  ListNode deleteDuplicates82All(ListNode head) {
+    if( head == null || head.next == null) return head;
+
+    ListNode start = new ListNode(-1);
+    start.next = head;
+    ListNode pre = start;
+    ListNode cur = head;
+    //consider two cases to complete full logics:
+    //  1 -> 1 -> 1-> 2-> 3-> 4
+    // 1 -> 2 -> 3 -> 4 -> 4 -> 4 then when finish while(cur.next != null && cur.val == cur.next.val)
+    // you will encounter the condition to tell if prev.next points to a dup node or regular node
+    while(cur != null) {// no need to have cur != null condition
+      while(cur.next != null && cur.val == cur.next.val)cur = cur.next;
+      if(pre.next == cur) {cur = cur.next; pre = pre.next; }
+      else { pre.next = cur.next; cur = pre.next;}
+    }
+    return start.next;
+  }
 }
