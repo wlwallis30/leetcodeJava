b02e28cbc3ad86138b4b417a80b20a68f851f5d2
wlwallis30 <wlwallis30@gmail.com>
2021-09-17 01:58:46 -0700

    adding 198, 213

--- src/main/java/com/wlwallis30/leetcode/MaxSubArray.java
+++ src/main/java/com/wlwallis30/leetcode/MaxSubArray.java
@@ -72,4 +72,73 @@ public class MaxSubArray {
 
     return globalMax;
   }
+
+  //转移方程 dp[i] = max(num[i] + dp[i - 2], dp[i - 1]), but you dont need array, just two variables enough
+  int rob198(int[] nums) {
+    if(nums.length == 0) return 0;
+    if(nums.length == 1) return nums[0];
+    int n = nums.length;
+    int[] maxRob = new int[n];
+    maxRob[0] = nums[0];
+    maxRob[1] = Math.max(nums[0], nums[1]);
+
+    for(int i = 2;i < n; ++i) maxRob[i] = Math.max(maxRob[i-1], maxRob[i-2]+nums[i]);
+    return maxRob[n-1];
+  }
+
+  int rob198Optimal(int[] nums) {
+    if(nums.length == 0) return 0;
+    if(nums.length == 1) return nums[0];
+    int n = nums.length;
+    // max for i-2 and i-1
+    int max_2 = nums[0];
+    int max_1 = Math.max(nums[0], nums[1]);
+    for(int i = 2; i < n; ++i) {
+      int tmpMax = Math.max(max_2 + nums[i], max_1);
+      max_2 = max_1;
+      max_1 = tmpMax;
+    }
+    return max_1;
+  }
+
+  //actually it is pretty much as same as max_2 and max_1
+  int rob198EvenOdd(int[] nums) {
+    if(nums.length == 0) return 0;
+    if(nums.length == 1) return nums[0];
+    int n = nums.length;
+    int robOdd = 0, robEven = 0;
+    for(int i = 0;i < n; ++i) {
+      // this algo will not work, coz you dont have to rob one after another, e.g. [2,1,1,100], you can rob 2, relax and 100
+      //if(i%2 == 0) robEven += nums[i];
+      //else robOdd += nums[i];
+      if (i % 2 == 0) robEven = Math.max(robEven + nums[i], robOdd);
+      else  robOdd = Math.max(robEven, robOdd + nums[i]);
+    }
+
+    return Math.max(robEven, robOdd);
+  }
+
+  // [7,4,1,9,3,8,6,5], --> [7,4,1,9,3,8,6] and [4,1,9,3,8,6,5]
+  public int robCircle213(int[] nums) {
+    if (nums.length == 0) return 0;
+    if (nums.length == 1) return nums[0];
+    int max1 = rob_simple(nums, 0, nums.length - 2);
+    int max2 = rob_simple(nums, 1, nums.length - 1);
+
+    return Math.max(max1, max2);
+  }
+
+  public int rob_simple(int[] nums, int start, int end) {
+    int max_1 = 0;
+    int max_2 = 0;
+
+    for (int i = start; i <= end; i++) {
+      int current = nums[i];
+      int tmpMax = Math.max(current + max_2, max_1);
+      max_2 = max_1;
+      max_1 = tmpMax;
+    }
+
+    return max_1;
+  }
 }
--- src/main/java/com/wlwallis30/leetcode/Solution.java
+++ src/main/java/com/wlwallis30/leetcode/Solution.java
@@ -59,7 +59,7 @@ public class Solution {
  * swap two nodes in pair 24:    ....total: 1
  * search insert position 35: 278, 34    ....total: 3
  * next pemutation 31: 46, 47     total: 3
- * max sub array 53: 121, 122, 309, 152    ....total: 5
+ * max sub array 53: 121, 122, 309, 152, 198, 213    ....total: 7
  * length of last word 58:      total: 1
  * sqrt of x 69: 367, 50,372     total: 4
  * climb chairs 70:     total: 1
@@ -112,7 +112,7 @@ public class Solution {
  * LRU cache 146: 460, 588   total: 3
  * reverse words 151: 186    total: 2
  * evaluate reversed poland notation 150:    total: 1
- *********************Similar problem catalogs: total: 180
+ *********************Similar problem catalogs: total: 182
  */
 // single-linked list where java LinkedList is a double linked list via Deque
 class ListNode {
